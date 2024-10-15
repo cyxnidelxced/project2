b2f6bb9df209eb0efa82a7a3fe9e76d0b6d28428
cyxnidelxced <jnguyen257@insite.4cd.edu>
2024-10-15 06:55:16 -0700

    update keyframe

--- js/main.js
+++ js/main.js
@@ -62,7 +62,7 @@ document.querySelector('.carousel').addEventListener('mouseleave', startAutoplay
 let images = document.querySelectorAll('.carousel-item');
 images.forEach((img) => {
     img.style.display="none";
-    img.style.animation="fade 1.5s ease-in-out";
+    img.style.animation="fade 400ms ease-in-out 1 alternate";
 });
 let left = document.querySelectorAll('.back');
 left.forEach((btn) => {
